11c8811bcf8cc6498e0b96a1d81fda9ceec70017
Joseph P. <joseph.pena.q@gmail.com>
2025-03-09 13:58:59 -0300

    refactor: use typer instead of argparse in run_awive

---
+++ awivec/from_distances.py
@@ -0,0 +1,50 @@
+import numpy as np
+from scipy.linalg import eigh
+
+
+def classical_mds(D, dim=2):
+    # D is the distance matrix (n x n)
+    n = D.shape[0]
+    # Create centering matrix
+    H = np.eye(n) - np.ones((n, n)) / n
+    # Square the distances
+    D_squared = D**2
+    # Apply double centering
+    B = -0.5 * H @ D_squared @ H
+    # Eigen decomposition: since B is symmetric, we can use eigh
+    eigvals, eigvecs = eigh(B)
+    # Sort eigenvalues and eigenvectors in descending order
+    idx = np.argsort(eigvals)[::-1]
+    eigvals = eigvals[idx][:dim]
+    eigvecs = eigvecs[:, idx][:, :dim]
+    # Compute coordinates using the positive eigenvalues
+    L = np.diag(np.sqrt(eigvals))
+    X = eigvecs @ L
+    return X
+
+
+# Example: Suppose we have 4 points with the following distance matrix:
+D = np.array(
+    [
+        [0, 3.7782, 19.3769, 20.6298],
+        [3.7782, 0, 19.0869, 21.8399],
+        [19.3769, 19.0869, 0, 8.3321],
+        [20.6298, 21.8399, 8.3321, 0],
+    ]
+)
+
+coords = classical_mds(D)
+print("Coordinates of the 4 points:")
+print(coords)
+# bring all to 0
+min_x = 0
+min_y = 0
+for coord in coords:
+    min_x = min(coord[0], min_x)
+    min_y = min(coord[1], min_y)
+print(f"{min_x=}, {min_y}")
+coords = list(map(lambda x: [float(round(x[0]-min_x, 2)), float(round(x[1]-min_y, 2))], coords))
+print("updated")
+for coord in coords:
+    print(coord)
+
--- awivec/run_awive.py
+++ awivec/run_awive.py
@@ -1,10 +1,17 @@
-import argparse
+import numpy as np
+import typer
 from awive.config import Config
 from awive.correct_image import Formatter
 from awive.loader import Loader, make_loader
 from awive.otv import OTV
+from typing_extensions import Annotated
 
-def run_otv(config_path: str, show_video=False, debug=0):
+
+def run_otv(
+    config_path: Annotated[str, typer.Argument(help="Path to configuration file path")],
+    show_video: Annotated[bool, typer.Option(help="Show video")] = False,
+    debug: Annotated[int, typer.Option(help="Debug level")] = 0,
+):
     """Basic example of OTV
 
     Processing for each frame
@@ -19,28 +26,16 @@ def run_otv(config_path: str, show_video=False, debug=0):
     config = Config.from_json(config_path)
     loader: Loader = make_loader(config.dataset)
     formatter = Formatter(config)
-    image = loader.read()
+    image: np.ndarray = loader.read()
     prev_gray = formatter.apply_distortion_correction(image)
     prev_gray = formatter.apply_roi_extraction(prev_gray)
     otv = OTV(config, prev_gray, debug)
-    return otv.run(loader, formatter, show_video)
+    x = otv.run(loader, formatter, show_video)
+    for key, value in x.items():
+        print(
+            f"{key}: velocity: {round(value['velocity'], 4)}, count: {value['count']}"
+        )
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "config_path",
-        help="Path to configuration file path",
-    )
-    parser.add_argument(
-        '-v',
-        '--show_video',
-        action='store_true',
-        help='Show video'
-    )
-    args = parser.parse_args()
-    x = run_otv(
-        config_path=args.config_path,
-        show_video=args.show_video,
-    )
-    for key, value in x.items():
-        print(f"{key}: velocity: {round(value['velocity'],4)}, count: {value['count']}")
+    typer.run(run_otv)

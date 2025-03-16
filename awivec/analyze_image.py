59e448f79c5029401f43f2ba2633146f3ff59d91
Joseph P. <joseph.pena.q@gmail.com>
2025-03-16 18:11:48 -0300

    refactor: update to awive 3

--- awivec/analyze_image.py
+++ awivec/analyze_image.py
@@ -1,33 +1,37 @@
 """Analyze image savig it as numpy file."""
 
-import argparse
+from pathlib import Path
 import logging
 
+from numpy.typing import NDArray
 from awive.config import Config
-from awive.correct_image import Formatter
+from awive.preprocess.correct_image import Formatter
 from awive.loader import Loader, make_loader
 import cv2
 import matplotlib.pyplot as plt
 from npyplotter.plot_npy import picshow
 import numpy as np
+import typer
 
 
 LOG = logging.getLogger(__name__)
 
 
 def main(
-    config_path: str,
-    entire_frame=False,
-    undistort=True,
-    roi=True,
-    get_frame=True,
-    plot=False,
+    config_fp: Path,
+    entire_frame: bool = False,
+    undistort: bool = False,
+    roi: bool = False,
+    get_frame: bool = False,
+    plot: bool = False,
 ) -> None:
     """Save the first image as numpy file."""
-    config = Config.from_json(config_path)
+    config = Config.from_fp(config_fp)
     loader: Loader = make_loader(config.dataset)
     formatter = Formatter(config)
-    image: np.ndarray = loader.read()
+    image: NDArray | None = loader.read()
+    if image is None:
+        raise ValueError("No image found")
     if get_frame:
         cv2.imwrite("image.png", image)
     if entire_frame:
@@ -36,6 +40,7 @@ def main(
         image = formatter.apply_distortion_correction(image)
     if roi:
         image = formatter.apply_roi_extraction(image)
+        # image = formatter.apply_resolution(image)
     if get_frame:
         cv2.imwrite("image.png", image)
     np.save("tmp.npy", image)
@@ -46,56 +51,57 @@ def main(
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        usage="python -m awive.analyze_image examples/datasets/river-brenta/config.json d0000 -P",
-        description=(
-            "Analyze image savig it as numpy file.\n"
-            "Order of processing:\n"
-            " 1. Crop image using gcp.pixels parameter\n"
-            " 2. If enabled, lens correction using preprocessing.image_correction\n"
-            " 3. Orthorectification using relation gcp.pixels and gcp.real\n"
-            " 4. Pre crop\n"
-            " 5. Rotation\n"
-            " 6. Crop\n"
-            " 7. Convert to gray scale\n"
-        ),
-    )
-    parser.add_argument(
-        "config_path",
-        help="Path to configuration file path",
-    )
-    parser.add_argument(
-        "-f", "--frame", action="store_true", help="Plot entire frame or not"
-    )
-    parser.add_argument(
-        "-u",
-        "--undistort",
-        action="store_true",
-        help="Format image using distortion correction",
-    )
-    parser.add_argument("-g", "--getframe", action="store_true", help="Get first frame")
-    parser.add_argument(
-        "-r",
-        "--roi",
-        action="store_true",
-        help="Format image using selecting only roi area",
-    )
-    parser.add_argument("-P", "--plot", action="store_true", help="Plot output image")
-    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
-    args = parser.parse_args()
-    logging.getLogger("matplotlib.font_manager").disabled = True
-    logging.getLogger("PIL.PngImagePlugin").disabled = True
-    logging.getLogger("matplotlib.pyplot").disabled = True
-    logging.basicConfig(
-        level=logging.DEBUG if args.debug else logging.INFO,
-        format="%(asctime)s | %(levelname).1s | %(name).20s | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    main(
-        config_path=args.config_path,
-        entire_frame=args.frame,
-        undistort=args.undistort,
-        roi=args.roi,
-        get_frame=args.getframe,
-        plot=args.plot,
-    )
+    typer.run(main)
+    # parser = argparse.ArgumentParser(
+    #     usage="python -m awive.analyze_image examples/datasets/river-brenta/config.json d0000 -P",
+    #     description=(
+    #         "Analyze image savig it as numpy file.\n"
+    #         "Order of processing:\n"
+    #         " 1. Crop image using gcp.pixels parameter\n"
+    #         " 2. If enabled, lens correction using preprocessing.image_correction\n"
+    #         " 3. Orthorectification using relation gcp.pixels and gcp.real\n"
+    #         " 4. Pre crop\n"
+    #         " 5. Rotation\n"
+    #         " 6. Crop\n"
+    #         " 7. Convert to gray scale\n"
+    #     ),
+    # )
+    # parser.add_argument(
+    #     "config_path",
+    #     help="Path to configuration file path",
+    # )
+    # parser.add_argument(
+    #     "-f", "--frame", action="store_true", help="Plot entire frame or not"
+    # )
+    # parser.add_argument(
+    #     "-u",
+    #     "--undistort",
+    #     action="store_true",
+    #     help="Format image using distortion correction",
+    # )
+    # parser.add_argument("-g", "--getframe", action="store_true", help="Get first frame")
+    # parser.add_argument(
+    #     "-r",
+    #     "--roi",
+    #     action="store_true",
+    #     help="Format image using selecting only roi area",
+    # )
+    # parser.add_argument("-P", "--plot", action="store_true", help="Plot output image")
+    # parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    # args = parser.parse_args()
+    # logging.getLogger("matplotlib.font_manager").disabled = True
+    # logging.getLogger("PIL.PngImagePlugin").disabled = True
+    # logging.getLogger("matplotlib.pyplot").disabled = True
+    # logging.basicConfig(
+    #     level=logging.DEBUG if args.debug else logging.INFO,
+    #     format="%(asctime)s | %(levelname).1s | %(name).20s | %(message)s",
+    #     datefmt="%Y-%m-%d %H:%M:%S",
+    # )
+    # main(
+    #     config_fp=args.config_path,
+    #     entire_frame=args.frame,
+    #     undistort=args.undistort,
+    #     roi=args.roi,
+    #     get_frame=args.getframe,
+    #     plot=args.plot,
+    # )
--- awivec/run_awive.py
+++ awivec/run_awive.py
@@ -1,16 +1,21 @@
-import numpy as np
+from pathlib import Path
+import logging
 import typer
 from awive.config import Config
-from awive.correct_image import Formatter
+from numpy.typing import NDArray
+from awive.preprocess.correct_image import Formatter
 from awive.loader import Loader, make_loader
-from awive.otv import OTV
+from awive.algorithms.otv import OTV
+from awive.algorithms.sti import STIV
 from typing_extensions import Annotated
 
 
 def run_otv(
-    config_path: Annotated[str, typer.Argument(help="Path to configuration file path")],
+    config_fp: Annotated[Path, typer.Argument(help="Path to configuration file path")],
     show_video: Annotated[bool, typer.Option(help="Show video")] = False,
-    debug: Annotated[int, typer.Option(help="Debug level")] = 0,
+    images_dp: Annotated[Path | None, typer.Option(help="Save images")] = None,
+    debug: Annotated[bool, typer.Option(help="Debug level")] = False,
+    algorithm: Annotated[str, typer.Option(help="otv or stiv")] = "otv",
 ):
     """Basic example of OTV
 
@@ -23,17 +28,45 @@ def run_otv(
         6. Crop
         7. Convert to gray scale
     """
-    config = Config.from_json(config_path)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname).1s | %(name).20s | %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    if debug:
+        logging.getLogger("awive.algorithms.otv").setLevel(logging.DEBUG)
+        logging.getLogger("awive.algorithms.sti").setLevel(logging.DEBUG)
+
+    config = Config.from_fp(config_fp)
     loader: Loader = make_loader(config.dataset)
     formatter = Formatter(config)
-    image: np.ndarray = loader.read()
+    image: NDArray | None = loader.read()
+    if image is None:
+        raise ValueError("No image found")
     prev_gray = formatter.apply_distortion_correction(image)
     prev_gray = formatter.apply_roi_extraction(prev_gray)
-    otv = OTV(config, prev_gray, debug)
-    x = otv.run(loader, formatter, show_video)
+    prev_gray = formatter.apply_resolution(prev_gray)
+
+    if algorithm == "otv":
+        otv = OTV(config, prev_gray, lines=config.lines, formatter=formatter)
+        x = otv.run(loader, show_video)
+    elif algorithm == "stiv" and config.stiv is not None:
+        stiv = STIV(
+            config=config.stiv,
+            loader=loader,
+            lines=config.lines,
+            formatter=formatter,
+            images_dp=images_dp,
+        )
+        x = stiv.run()
+    else:
+        raise ValueError("Algorithm not found")
     for key, value in x.items():
         print(
-            f"{key}: velocity: {round(value['velocity'], 4)}, count: {value['count']}"
+            f"{key}: "
+            f"velocity: {round(value['velocity'], 4)}, "
+            f"count: {value.get('count', 0)}, "
+            f"position: {value.get('position', 0)}"
         )
 
 
